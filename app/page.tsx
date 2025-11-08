6cb95e9655d6c7f887fc521ddbbde7d9d061a1f1
A Lalith <lalithsep394@gmail.com>
2025-11-08 22:10:07 +0530

    Add button component for future primary usage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
-
+import Button from "./components/button";
 export default function Home() {
   return (
     <div>
       <div className="bg-blue-300 text-slate-700 text-4xl p-5">
         Hi. Welcome to the webpage
       </div>
+      <Button></Button>
     </div>
   );
 }
